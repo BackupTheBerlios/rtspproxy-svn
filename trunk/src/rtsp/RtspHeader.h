309cc51f75763ddf5a8aba9b013da27aa6d72713
mat <mat@858dd99d-b9f7-0310-8e3e-9744f7518187>
2005-06-04 10:38:13 +0000

    Deleted lib/* and added header info

--- trunk/src/rtsp/RtspHeader.h
+++ trunk/src/rtsp/RtspHeader.h
@@ -1,4 +1,26 @@
-
+/**************************************************************************
+ *   Copyright (C) 2005 Matteo Merli <matteo.merli@gmail.com>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *   $Id$
+ * 
+ *   $URL$
+ * 
+ *****************************************************************************/
+ 
 #ifndef _RTSP_HEADER_H_
 #define _RTSP_HEADER_H_
 
