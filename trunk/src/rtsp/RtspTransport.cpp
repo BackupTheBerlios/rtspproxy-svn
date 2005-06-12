9d8067b8e50c63964b9e3ac6bc72f0ef39746b82
mat <mat@858dd99d-b9f7-0310-8e3e-9744f7518187>
2005-06-12 08:48:48 +0000

    Finished RtspTransport class.

--- trunk/src/rtsp/RtspTransport.cpp
+++ trunk/src/rtsp/RtspTransport.cpp
@@ -54,6 +54,246 @@
    Example:
      Transport: RTP/AVP;multicast;ttl=127;mode="PLAY",
                 RTP/AVP;unicast;client_port=3456-3457;mode="PLAY"
- 
  */
+ 
+#include <QtCore>
+ 
+RtspTransports::RtspTransports( const QString& str )
+{
+	QStringList list = str.split( "," );
+ 	QStringListIterator it( list );
+ 	while ( it.hasNext() ) {
+ 		transports << *( new RtspTransport( it.next() ) );
+ 	}
+}
+ 
+RtspTransports::~RtspTransports()
+{
+}
+
+QString RtspTransports::toString() const
+{
+	QStringList list;
+	RtspTransportListIterator it( transports );
+	while ( it.hasNext() ) {
+		list << it.next().toString();
+	}
+	return list.join(",");
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+
+
+RtspTransport::TransportProtocol RtspTransport::getTransportProtocol( const QString& str )
+{
+	if ( str == "RTP" ) return RTP;
+	if ( str == "RDT" ) return RDT;
+	
+	return TransportProtocolNone;	
+}
+
+QString RtspTransport::getTransportProtocol( TransportProtocol p )
+{
+	switch ( p ) {
+		case RTP : return "RTP";
+		case RDT : return "RDT";
+		default: return "";
+	};	
+}
+
+RtspTransport::Profile RtspTransport::getProfile( const QString& str )
+{
+	if ( str == "AVP" ) return AVP;
+	
+	return ProfileNone;
+}
+
+QString RtspTransport::getProfile( Profile p )
+{
+	switch ( p ) {
+		case AVP : return "AVP";
+		default: return "";
+	};
+}
+
+RtspTransport::LowerTransport RtspTransport::getLowerTransport( const QString& str )
+{
+	if ( str == "TCP" ) return TCP;
+	if ( str == "UDP" ) return UDP;
+	return LowerTransportNone;
+}
+
+QString RtspTransport::getLowerTransport( LowerTransport p )
+{
+	switch ( p ) {
+		case TCP : return "TCP";
+		case UDP : return "UDP";
+		default  : return "";
+	};
+}
+
+RtspTransport::DeliveryType RtspTransport::getDeliveryType( const QString& str )
+{
+	if ( str == "unicast" ) return UNICAST;
+	if ( str == "multicast" ) return MULTICAST;
+	return DeliveryTypeNone;
+}
+
+QString RtspTransport::getDeliveryType( DeliveryType p )
+{
+	switch ( p ) {
+		case UNICAST : return "unicast";
+		case MULTICAST : return "multicast";
+		default: return "";
+	};
+}
+
+
+QString _getStrValue( const QString& str )
+{
+	QStringList list = str.split( "=" );
+	if ( list.count() != 2 )
+		return QString::null;
+		
+	return list.at( 1 );
+}
+
+Int16Pair _getPairValue( const QString& str )
+{
+	Int16Pair pair;
+	pair.first = 0; pair.second = 0;
+	QStringList list = str.split( "=" );
+	if ( list.count() != 2 )
+		return pair;
+	pair.first = list.at( 1 ).section( "-", 0, 0 ).toInt();
+	pair.second = list.at( 1 ).section( "-", 1, 1 ).toInt();
+	return pair;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+
+
+RtspTransport::RtspTransport( const QString& str )
+{
+	qDebug() << "New Transport:" << str;
+	transport_protocol = TransportProtocolNone;
+	profile = ProfileNone;
+	lower_transport = LowerTransportNone;
+	delivery_type = DeliveryTypeNone;
+	destination = QString::null;
+	interleaved = QString::null;
+	layers = 0;
+	append = false;
+	ttl = 0;
+	port.first = 0; port.second = 0;
+	client_port.first = 0; client_port.second = 0;
+	server_port.first = 0; server_port.second = 0;
+	ssrc = QString::null;
+	mode = QString::null;
+	
+	parseTransport( str );
+}
+
+RtspTransport::~RtspTransport()
+{
+}
+ 
+void RtspTransport::parseTransport( const QString& transport )
+{
+	QStringList tokens = transport.split( ";" );
+	QStringListIterator it( tokens );
+	while ( it.hasNext() ) {
+		QString tok = it.next();
+		
+		// First check for the transport protocol
+		if ( tok.startsWith( "RTP" ) || tok.startsWith( "RDT" ) ) {
+			QStringList tpl = tok.split( "/" );
+			transport_protocol = getTransportProtocol( tpl.at( 0 ) );
+			if ( tpl.count() > 1 )
+				profile = getProfile( tpl.at( 1 ) );
+			if ( tpl.count() > 2 )
+				lower_transport = getLowerTransport( tpl.at( 2 ) );
+			continue;
+		}
+		
+		if ( tok == "unicast" )
+			setDeliveryType( UNICAST );
+		else if ( tok == "multicast" ) 
+			setDeliveryType( MULTICAST );
+		else if ( tok.startsWith( "destination" ) ) 
+			setDestination( _getStrValue( tok ) );
+		else if ( tok.startsWith( "interleaved" ) )
+			setInterleaved( _getStrValue( tok ) );
+		else if ( tok.startsWith( "append" ) )
+			setAppend( true );
+		else if ( tok.startsWith( "layers" ) )
+			setLayers( _getStrValue( tok ).toInt() );
+		else if ( tok.startsWith( "ttl" ) )
+			setTTL( _getStrValue( tok ).toInt() );
+		else if ( tok.startsWith( "port" ) )
+			setPort( _getPairValue( tok ) );
+		else if ( tok.startsWith( "client_port" ) )
+			setClientPort( _getPairValue( tok ) );
+		else if ( tok.startsWith( "server_port" ) )
+			setServerPort( _getPairValue( tok ) );
+		else if ( tok.startsWith( "ssrc" ) ) 
+			setSSRC( _getStrValue( tok ) );
+		else if ( tok.startsWith( "mode" ) ) 
+			setMode( _getStrValue( tok ) );
+	}
+}
+
+QString RtspTransport::toString() const
+{
+	QString res;
+	QTextStream out( &res );
+	out << getTransportProtocol( transport_protocol );
+	if ( profile != ProfileNone )
+		out << "/" << getProfile( profile );
+	if ( lower_transport != LowerTransportNone )
+		out << "/" << getLowerTransport( lower_transport );
+	if ( delivery_type != DeliveryTypeNone )
+		out << ";" << getDeliveryType( delivery_type );
+	if ( destination != QString::null )
+		out << ";destination=" << destination;
+	if ( interleaved != QString::null )
+		out << ";interleaved=" << interleaved;
+	if ( append )
+		out << ";append";
+	if ( layers )
+		out << ";layers=" << layers;
+	if ( ttl )
+		out << ";ttl=" << ttl;
+	if ( port.first )
+		out << ";port=" << port.first << "-" << port.second;
+	if ( client_port.first )
+		out << ";client_port=" << client_port.first << "-" << client_port.second;
+	if ( server_port.first )
+		out << ";server_port=" << server_port.first << "-" << server_port.second;
+	if ( ssrc != QString::null )
+		out << ";ssrc=" << ssrc;
+	if ( mode != QString::null )
+		out << ";mode=" << mode;
+	return res;
+}
+
+
+#ifdef TEST_RTSP_TRANSPORT
+
+int main( int argc, char** argv )
+{
+	if ( argc < 2 )
+		return;
+	RtspTransports *t = new RtspTransports( argv[ 1 ] );
+	
+	qDebug() << "RESULT:";
+	qDebug() << t->toString();
+}
+
+#endif
+
  
--- trunk/src/rtsp/RtspTransport.h
+++ trunk/src/rtsp/RtspTransport.h
@@ -23,13 +23,118 @@
  
 #ifndef _RTSP_TRANSPORT_H_
 #define _RTSP_TRANSPORT_H_
- 
+
+#include <QtCore>
+
+typedef QPair<quint16,quint16> Int16Pair;
+
 class RtspTransport
 {
 public:
+	enum TransportProtocol {
+		TransportProtocolNone,
+		RTP,
+		RDT
+	};
 
+	enum Profile {
+		ProfileNone,
+		AVP
+	};
+	
+	enum LowerTransport {
+		LowerTransportNone,
+		TCP,
+		UDP
+	};
+	
+	enum DeliveryType {
+		DeliveryTypeNone,
+		UNICAST,
+		MULTICAST
+	};
+
+	RtspTransport( const QString& str );
+	~RtspTransport();
+	
+	// Getters
+	TransportProtocol getTransportProtocol() const { return transport_protocol; }
+	Profile getPofile() const { return profile; }
+	LowerTransport getLowerTransport() const { return lower_transport; }
+	DeliveryType getDeliveryType() const { return delivery_type; }
+	QString getDestination() const { return destination; }
+	QString getInterleaved() const { return interleaved; }
+	bool getAppend() const { return append; }
+	quint32 getLayers() const { return layers; }
+	quint32 getTTL() const { return ttl; }
+	Int16Pair getPort() const { return port; }
+	Int16Pair getClientPort() const { return client_port; }
+	Int16Pair getServerPort() const { return server_port; }
+	QString getSSRC() const { return ssrc; }
+	QString getMode() const { return mode; }
+	
+	// Setters
+	void setTransportProtocol( TransportProtocol transport_protocol ) { this->transport_protocol = transport_protocol; }
+	void setProfile( Profile profile ) { this->profile = profile; }
+	void setLowerTransport( LowerTransport lower_transport ) { this->lower_transport = lower_transport; }
+	void setDeliveryType( DeliveryType delivery_type ) { this->delivery_type = delivery_type; }
+	void setDestination( const QString& destination ) { this->destination = destination; }
+	void setInterleaved( const QString& interleaved ) { this->interleaved = interleaved; }
+	void setAppend( bool append ) { this->append = append; }
+	void setLayers( quint32 layers ) { this->layers = layers; }
+	void setTTL( quint32 ttl ) { this->ttl = ttl; }
+	void setPort( const Int16Pair& port ) { this->port = port; }
+	void setClientPort( const Int16Pair& client_port ) { this->client_port = client_port; }
+	void setServerPort( const Int16Pair& server_port ) { this->server_port = server_port; }
+	void setSSRC( const QString& ssrc ) { this->ssrc = ssrc; }
+	void setMode( const QString& mode ) { this->mode = mode; }
+	
+	QString toString() const;
+	
 private:
+	void parseTransport( const QString& transport );
 
+	TransportProtocol transport_protocol;
+	Profile profile;
+	LowerTransport lower_transport;
+	DeliveryType delivery_type;
+	
+	QString destination;
+	QString interleaved;
+	qint32 layers;
+	bool append;
+	quint32 ttl;
+	Int16Pair port;
+	Int16Pair client_port;
+	Int16Pair server_port;
+	QString ssrc;
+	QString mode;
+	
+	
+	//// Converters between enums and QStrings
+	static TransportProtocol getTransportProtocol( const QString& str );
+	static QString getTransportProtocol( TransportProtocol p );
+	static Profile getProfile( const QString& str );
+	static QString getProfile( Profile p );
+	static LowerTransport getLowerTransport( const QString& str );
+	static QString getLowerTransport( LowerTransport p );
+	static DeliveryType getDeliveryType( const QString& str );
+	static QString getDeliveryType( DeliveryType p );
+};
+
+typedef QList<RtspTransport> RtspTransportList;
+typedef QListIterator<RtspTransport> RtspTransportListIterator;
+ 
+class RtspTransports
+{
+public:
+	RtspTransports( const QString& str );
+	~RtspTransports();
+	
+	QString toString() const;
+	
+private:
+	RtspTransportList transports;
 };
  
 #endif // _RTSP_TRANSPORT_H_
