ed94e09c1181547b2598e21cb8656e6499d0077b
mat <mat@858dd99d-b9f7-0310-8e3e-9744f7518187>
2005-06-18 22:56:09 +0000

    Added RtspTransport class

---
+++ branches/java/rtspproxy/rtsp/RtspTransport.java
@@ -0,0 +1,36 @@
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   Copyright (C) 2005 - Matteo Merli - matteo.merli@gmail.com            *
+ *                                                                         *
+ ***************************************************************************/
+
+/*
+ * $Id$
+ * 
+ * $URL$
+ * 
+ */
+
+package rtspproxy.rtsp;
+
+
+/**
+ * Parse the RTSP Transport header field.
+ */
+public class RtspTransport
+{
+
+	/**
+	 * Constructor. Creates a RtspTransport object from a transport
+	 * header string. 
+	 */
+	public RtspTransport()
+	{
+	}
+
+}
