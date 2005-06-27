9efa18a88ae0aadbec78ef4e423112104e201ce9
mat <mat@858dd99d-b9f7-0310-8e3e-9744f7518187>
2005-06-27 22:21:22 +0000

    Started using log4j

--- branches/java/rtspproxy/Main.java
+++ branches/java/rtspproxy/Main.java
@@ -17,21 +17,36 @@
  */
 package rtspproxy;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
 /**
  * 
  */
 public class Main
 {
 
+	static Logger log = Logger.getRootLogger(  );
+
 	public static void main( String[] args )
 	{
 		// TODO: remove this temp stuffs
-		for ( Object key : System.getProperties().keySet() ) {
-			String value = System.getProperty( (String)key );
-			System.out.println( key + " : " + value );
-		}
-
+		/*
+		 for ( Object key : System.getProperties().keySet() ) {
+		 String value = System.getProperty( (String)key );
+		 System.out.println( key + " : " + value );
+		 }
+		 */
+		/*Properties prop = new Properties();
+		prop.setProperty( "log4j.rootLogger", "DEBUG, A1" );
+		prop.setProperty( "log4j.appender.A1", "org.apache.log4j.ConsoleAppender" );
+		prop.setProperty( "log4j.appender.A1.layout", "org.apache.log4j.PatternLayout" );
+		prop.setProperty( "log4j.appender.stdout.layout.ConversionPattern", "%5p [%t] (%F:%L) - %m%n" );
+		PropertyConfigurator.configure( prop );
+		*/ BasicConfigurator.configure();
+		log.warn( "Try.." );
 		try {
+			log.warn( "Starting.." );
 			Reactor reactor = new Reactor();
 
 		} catch ( Exception e ) {
