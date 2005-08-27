87b6800bac4613d7f17f3c6cde4c3924a6122f99
merlimat <merlimat@858dd99d-b9f7-0310-8e3e-9744f7518187>
2005-08-27 10:27:02 +0000

    Added preliminary RTP/RTCP packet handling

---
+++ branches/java/src/main/java/rtspproxy/rtp/Packet.java
@@ -0,0 +1,32 @@
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   Copyright (C) 2005 - Matteo Merli - matteo.merli@gmail.com            *
+ *                                                                         *
+ ***************************************************************************/
+
+/*
+ * $Id$
+ * 
+ * $URL$
+ * 
+ */
+
+package rtspproxy.rtp;
+
+import java.nio.ByteBuffer;
+
+/**
+ * Base interface for RTP and RTCP packets
+ * 
+ * @author mat
+ */
+public interface Packet
+{
+
+	public ByteBuffer toByteBuffer();
+}
