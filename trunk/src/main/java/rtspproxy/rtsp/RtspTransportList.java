46a5b51d9d6408f143f09e0c11c2fc39a951ad45
merlimat <merlimat@858dd99d-b9f7-0310-8e3e-9744f7518187>
2005-12-08 21:19:56 +0000

    Reverted unwanted deletion of trunk/

---
+++ trunk/src/main/java/rtspproxy/rtsp/RtspTransportList.java
@@ -0,0 +1,74 @@
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   Copyright (C) 2005 - Matteo Merli - matteo.merli@gmail.com            *
+ *                                                                         *
+ ***************************************************************************/
+
+/*
+ * $Id$
+ * 
+ * $URL$
+ * 
+ */
+
+package rtspproxy.rtsp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Represent a list of transport headers.
+ */
+public class RtspTransportList
+{
+
+	private List<RtspTransport> transportList;
+
+	/**
+	 * Constructor. Creates a list of transport type.
+	 */
+	public RtspTransportList( String transportHeader )
+	{
+		transportList = new ArrayList<RtspTransport>();
+
+		for ( String transport : transportHeader.split( "," ) ) {
+			transportList.add( new RtspTransport( transport ) );
+		}
+	}
+
+	public List<RtspTransport> getList()
+	{
+		return transportList;
+	}
+
+	public RtspTransport get( int index )
+	{
+		return transportList.get( index );
+	}
+
+	/**
+	 * @return The number of transports defined.
+	 */
+	public int count()
+	{
+		return transportList.size();
+	}
+
+	public String toString()
+	{
+		StringBuilder buf = new StringBuilder();
+		int i = 0;
+		for ( RtspTransport t : transportList ) {
+			if ( i++ != 0 )
+				buf.append( "," );
+			buf.append( t.toString() );
+		}
+		return buf.toString();
+	}
+
+}
